632e0726db7e8aee4f9ad4bec8f568827ddca25d
Holden Karau <holden@pigscanfly.ca>
2018-07-18 14:19:39 -0700

    Cache data uploads

--- examples/chicago_taxi/preprocess_flink.sh
+++ examples/chicago_taxi/preprocess_flink.sh
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 set -u
+set -x
 
 echo Starting flink TFT preprocessing...
 
@@ -22,8 +23,9 @@ if [ -z "$MYBUCKET" ]; then
   exit 1
 fi
 
-JOB_ID="chicago-taxi-preprocess-$(date +%Y%m%d-%H%M%S)"
-JOB_INPUT_PATH=$MYBUCKET/$JOB_ID/chicago_taxi_input
+DEFAULT_JOB_ID="chicago-taxi-preprocess-$(date +%Y%m%d-%H%M%S)"
+JOB_ID="${JOB_ID:-$DEFAULT_JOB_ID}"
+JOB_INPUT_PATH=$MYBUCKET/input/chicago_taxi_input
 JOB_OUTPUT_PATH=$MYBUCKET/$JOB_ID/chicago_taxi_output
 export TFT_OUTPUT_PATH=$JOB_OUTPUT_PATH/tft_output
 TEMP_PATH=$MYBUCKET/$JOB_ID/tmp/
@@ -49,14 +51,14 @@ echo TFT output path: $TFT_OUTPUT_PATH
 END
 
 # move data to gcs
-echo Uploading data to GCS
-gsutil cp -r ./data/eval/ ./data/train/ $JOB_INPUT_PATH/
+echo Uploading data to GCS if not uploaded
+gsutil -q stat $JOB_INPUT_PATH/eval/* || gsutil cp -r ./data/eval/ ./data/train/ $JOB_INPUT_PATH/
 
 
 # Preprocess the eval files
 echo Preprocessing eval data...
 rm -R -f $(pwd)/data/eval/local_chicago_taxi_output
-python preprocess.py \
+time python preprocess.py \
   --output_dir $JOB_OUTPUT_PATH/eval/local_chicago_taxi_output \
   --outfile_prefix eval_transformed \
   --input $JOB_INPUT_PATH/eval/data.csv \
@@ -65,10 +67,14 @@ python preprocess.py \
   --runner PortableRunner \
   --job_endpoint=localhost:8099 --sdk_location=container
 
+echo Waiting...
+
+sleep 10
+
 # Preprocess the train files, keeping the transform functions
 echo Preprocessing train data...
 rm -R -f $(pwd)/data/train/local_chicago_taxi_output
-python preprocess.py \
+time python preprocess.py \
   --output_dir $JOB_OUTPUT_PATH/train/local_chicago_taxi_output \
   --outfile_prefix train_transformed \
   --input $JOB_INPUT_PATH/train/data.csv \
@@ -76,4 +82,3 @@ python preprocess.py \
   --experiments=beam_fn_api \
   --runner PortableRunner \
   --job_endpoint=localhost:8099 --sdk_location=container
-
